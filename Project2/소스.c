1b36895bb3b2a77a29fc492e6a5fa69043a4a9f8
sangyun01 <worldtop0612@gmail.com>
2024-12-31 01:42:04 +0900

    pr

--- Project2/소스.c
+++ Project2/소스.c
@@ -18,12 +18,6 @@ int main(void)
 	printf("첫 번째 점의 좌표를 입력하시오: ");
 	scanf("%lf %lf", &x1, &y1);
 
-	printf("첫 번째 점의 좌표를 입력하시오: ");
-	scanf("%lf %lf", &x2, &y2);
-
-	x = distance(x1, x2, y1, y2);
-
-	printf("두 점 사이의 거리는 %lf입니다.", x);
 
 	return 0;
 }
